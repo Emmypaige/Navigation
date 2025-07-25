5cec28a865dc8a0d96428260ba72fafb7b7ae31a
emmy <emmyphall@hmail.com>
2025-07-25 10:08:11 -0500

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Morgan Hall on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
